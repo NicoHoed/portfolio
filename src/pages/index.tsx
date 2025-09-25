078a7dcdafe25f08babedb093c6466bb9dd5297a
NicoHoed <nico.hoed@gmail.com>
2025-09-25 19:08:56 +0200

    1er commit : page blanche

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen text-center p-6">
+      <h1 className="text-4xl font-bold mb-4">
+        Salut, je suis{" "}
+        <span className="text-blue-600">Nicolas Hoedenaeken</span> 👋
+      </h1>
+      <p className="text-lg text-gray-700 max-w-xl">
+        Bienvenue sur mon portfolio ✨
+      </p>
+    </main>
+  );
+}
